ee25edf932829f86e59dfaa662e5ad8619f21ecd
Abhishek <66775793+thedarkknight99@users.noreply.github.com>
2022-09-10 19:18:22 +0530

    added bonus feature

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cashGiven = document.querySelector("#cash-given");
 const checkBtn = document.querySelector("#check-btn");
 const message = document.querySelector("#errormessage");
 const numOfNotes = document.querySelectorAll(".no-of-notes");
+const totalCashGiveDiv = document.querySelector(".totalCashGiven");
+const nextBtn = document.querySelector("#next");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 function validateBillAmount() {
@@ -10,6 +12,7 @@ function validateBillAmount() {
     if (billAmount.value > 0) {
         if (cashGiven.value >= billAmount.value) {
             const amountToReturned = cashGiven.value - billAmount.value;
+            message.style.display = "none"
             calculateChange(amountToReturned);
         }
         else {
@@ -31,5 +34,21 @@ function calculateChange(amountToReturned) {
         numOfNotes[i].innerText = numberOfNotes;
     }
 }
+function hideCashGiven() {
+    totalCashGiveDiv.style.display = "none"
+}
+function showCashDiv() {
+    totalCashGiveDiv.style.display = "flex"
+}
+function nextValidate() {
+    if (billAmount.value > 0) {
+        nextBtn.style.display = "none"
+        showCashDiv()
+    }
+    else {
+        showMessage("Invalid Bill Amount")
+    }
+}
 
 checkBtn.addEventListener("click", validateBillAmount);
+nextBtn.addEventListener("click", nextValidate);
